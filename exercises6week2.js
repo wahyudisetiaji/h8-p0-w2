61f23b9bda7b0a6824eaf0e402b99292a2ce2190
Wahyudi Setiaji <wahyudisetiaji@yahoo.com>
2018-02-08 10:17:13 +0700

    Update Exercises 6 Week 2

--- exercises6week2.js
+++ exercises6week2.js
@@ -1,4 +1,5 @@
-// [Exercises 6 - Week 2] Berlatih Perulangan di JavaScript
+// LOOPING
+
 // 1. Melakukan Looping Menggunakan While
 
 var indeks = 2;
@@ -23,9 +24,6 @@ while (indeks > limit2) {
   indeks -= increment;
 }
 
-
-
-
 // 2. Melakukan Looping Menggunakan For
 
 var indeks = 2;
@@ -40,6 +38,7 @@ for(indeks; indeks <= limit1; indeks += increment) {
   console.log(indeks + ' I Love codeng');
 }
 
+console.log('LOOPING KEDUA')
 indeks = indeks - 2;
 
 // KONDISI FOR 2
@@ -47,66 +46,54 @@ for(indeks; indeks > limit2; indeks -= increment) {
   console.log(indeks + ' I will become fullstack developer')
 }
 
+// 3. Angka Ganjil dan Genap
+
+// Buatlah sebuah perulangan 1 - 100 dengan pertambahan counter sebanyak 1
+// Di dalam perulangan, periksa setiap angka counter:
+//     Apabila angka counter adalah angka genap, tuliskan GENAP
+//     Apabila angka counter adalah angka ganjil, tuliskan GANJIL
 
 
+var num = 99;
 
-//3. Angka Ganjil dan Genap
-//Problem 1 (Perulangan 1 - 100)
-
-var flag = 1;
-
-while(flag <= 100) {
-    console.log(flag + ' - Counter');
-    flag++;
+for(i = num; i <= 100; i++){
+  if((i % 2) === 0){
+    console.log('Genap');
+  }else{
+    console.log('Ganjil');
+  }
 }
 
+// Buatlah 3 perulangan baru dari 1 - 100, dengan pertambahan counter sebesar 2, 5, dan 9.
+// Pada 3 perulangan baru ini periksa setiap angka counter:
+//     Apabila bukan kelipatan yang ditentukan tidak perlu menuliskan apa-apa
+//     Apabila angka counter adalah kelipatan 3 dengan pertambahan 2,
+//     kelipatan 6 dengan pertambahan 5, dan kelipatan 10 dengan pertambahan 9, tuliskan:
+//       "3 kelipatan 3" dan seterusnya.
 
-//Problem 2 (Ganjil & Genap)
+// Untuk pertambahan counter 2
 
-var flag = 101;
-
-while(flag > 0) {
-  flag = flag-2;
+for(i = 1; i <=100; i+=2){
+  var counter = i;
+  if((counter % 3)=== 0){
+    console.log('3 KELIPATAN 3');
+  }
 }
-if (flag === 0)
-    {console.log('Genap')}
-  else
-    {console.log('Ganjil')}
 
+// Untuk pertambahan counter 5
 
-//Problem 3 (Counter 2)
-
-var flag = 9;
-
-while(flag > 0) {
-  flag = flag-3;
+for(i = 1; i <=100; i+=5){
+  var counter = i;
+  if((counter % 6)=== 0){
+    console.log('6 KELIPATAN 6');
+  }
 }
-if (flag === 0)
-    {console.log('3 kelipatan 3')}
-  else
-    {console.log('')}
 
-//Problem 3 (Counter 5)
+// Untuk pertambahan counter 9
 
-var flag = 1;
-
-while(flag > 0) {
-  flag = flag-6;
+for(i = 1; i <=100; i+=9){
+  var counter = i;
+  if((counter % 10)=== 0){
+    console.log('10 KELIPATAN 10');
+  }
 }
-if (flag === 0)
-    {console.log('6 kelipatan 6')}
-  else
-    {console.log('')}
-
-
-//Problem 3 (Counter 9)
-
-var flag = 110;
-
-while(flag > 0) {
-  flag = flag-10;
-}
-if (flag === 0)
-    {console.log('10 kelipatan 10')}
-  else
-    {console.log('')}
